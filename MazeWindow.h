28e668a2c9241d9f851c445b0abdfbc034366781
Alex Morton-Cooke <mortoniumcook@gmail.com>
2013-11-28 14:53:11 +0000

    Filled MazeWindow with adapted sample code Sample code for making OpenGL context windows was taken from Qt's website and adapted into MazeWindow. Code for drawing the triangle in the sample is commented out.

--- MazeWindow.h
+++ MazeWindow.h
@@ -1,10 +1,50 @@
 #ifndef MAZEWINDOW_H
 #define MAZEWINDOW_H
 
-class MazeWindow
+#include <QWindow>
+//#include <QtCore>
+#include <QExposeEvent>
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+#include <QOpenGLPaintDevice>
+#include <QOpenGLShaderProgram>
+
+class MazeWindow : public QWindow, protected QOpenGLFunctions
 {
+    Q_OBJECT
+
+private:
+    bool m_update_pending;
+    bool m_animating;
+
+    QOpenGLContext *m_context;
+    QOpenGLPaintDevice *m_device;
+
+protected:
+    bool event(QEvent *event);
+
+    void exposeEvent(QExposeEvent *event);
+    void resizeEvent(QResizeEvent *event);
+
 public:
-    MazeWindow();
+    explicit MazeWindow(QWindow *parent = 0);
+    ~MazeWindow();
+
+    virtual void render(QPainter *painter);
+    virtual void render();
+
+    virtual void initialize();
+
+    void setAnimating(bool animating);
+
+    GLuint loadShader(GLenum type, const char *source);
+    QOpenGLShaderProgram *m_program;
+
+public slots:
+    void renderLater();
+    void renderNow();
+
+
 };
 
 #endif // MAZEWINDOW_H
