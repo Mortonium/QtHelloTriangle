10b1be02d9cad184fd300d37a8298ead63bde1e8
Mortonium <mortoniumcook@gmail.com>
2013-12-04 13:08:21 +0000

    Made Position class Practically identical to the Size class atm

--- position.cpp
+++ position.cpp
@@ -1,5 +1,18 @@
 #include "position.h"
 
-Position::Position()
-{
+Position::Position(int x, int y, int z) {
+	itsX = x;
+	itsY = y;
+	itsZ = z;
 }
+
+int Position::getX() {
+	return itsX;
+}
+int Position::getY() {
+	return itsY;
+}
+int Position::getZ() {
+	return itsZ;
+}
+
--- position.h
+++ position.h
@@ -1,10 +1,20 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-class Position
-{
+class Position {
+
+private:
+	int itsX;
+	int itsY;
+	int itsZ;
+
 public:
-    Position();
+	Position(int x, int y, int z);
+
+	int getX();
+	int getY();
+	int getZ();
+
 };
 
 #endif // POSITION_H
