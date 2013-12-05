78acb74e2aaf9b5549dd750973a47ad1a2ca85e2
Alex Morton-Cooke <mortoniumcook@gmail.com>
2013-12-05 14:46:29 +0000

    Build skeleton for Maze3D Most of the method declarations, excepting the Cell getters. No implementations, just empty method blocks.

--- maze3d.h
+++ maze3d.h
@@ -1,10 +1,27 @@
 #ifndef MAZE3D_H
 #define MAZE3D_H
 
-class Maze3D
-{
+#include <string>
+
+#include "size.h"
+
+class Maze3D {
+	
+private:
+	Size itsSize;
+	
 public:
-    Maze3D();
+	Maze3D(Size size);
+	
+	Size getSize();
+	
+	static Maze3D generate(std:string script);
+	static Maze3D generateFromFile(std:string file);
+	virtual bool generateActual(std::string script) = 0;
+	
+	static Maze3D load(std::string file);
+	bool save(std::string file);
+	
 };
 
 #endif // MAZE3D_H
