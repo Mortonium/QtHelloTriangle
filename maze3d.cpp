79619848c889590a85a789b823bdbe4ab1e746e4
Alex Morton-Cooke <mortoniumcook@gmail.com>
2013-12-13 20:55:04 +0000

    Very simple, hard-coded maze

--- maze3d.cpp
+++ maze3d.cpp
@@ -1,4 +1,5 @@
 #include "maze3d.h"
+#include "maze3dbooleanarray.h"
 
 Maze3D::Maze3D(Size size) : itsSize(size) {
 	
@@ -13,6 +14,11 @@ Maze3D* Maze3D::generate(std::string script) {
 }
 Maze3D* Maze3D::generateFromFile(std::string file) {
 	
+}
+Maze3D* Maze3D::generateTestMaze() {
+	Maze3DBooleanArray* maze = new Maze3DBooleanArray(Size(5, 5, 5));
+	maze->generateTestMaze();
+	return (Maze3D*)maze;
 }
 
 Maze3D* Maze3D::load(std::string file) {
