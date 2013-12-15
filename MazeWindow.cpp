6f53c17640ba81dde02b16568ef4be59579cfe5e
Alex Morton-Cooke <mortoniumcook@gmail.com>
2013-12-15 16:31:39 +0000

    Removed redundant GLSL source variables

--- MazeWindow.cpp
+++ MazeWindow.cpp
@@ -1,21 +1,5 @@
 #include "MazeWindow.h"
 
-static const char *vertexShaderSource =
-	"attribute highp vec4 posAttr;\n"
-	"attribute lowp vec4 colAttr;\n"
-	"varying lowp vec4 col;\n"
-	"uniform highp mat4 matrix;\n"
-	"void main() {\n"
-	"col = colAttr;\n"
-	"gl_Position = matrix * posAttr;\n"
-	"}\n";
-
-static const char *fragmentShaderSource =
-	"varying lowp vec4 col;\n"
-	"void main() {\n"
-	"gl_FragColor = col;\n"
-	"}\n";
-
 MazeWindow::MazeWindow(QWindow *parent)
 	: QWindow(parent)
 	, m_update_pending(false)
